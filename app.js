1a564c800f02e9a66317cb6e9289d7d18115813c
MNischida <mgonischida@gmail.com>
2023-04-04 19:44:02 -0300

    fazendo requisicao

--- app.js
+++ app.js
@@ -55,7 +55,32 @@ app.post('/stop', (req, res) => {
 
 // Execute
 app.post('/execute', (req, res) => {
-    console.log(res.status(200).json());
+    const request = req.body;
+    var url = 'https://en1mbrh7056kqcg.m.pipedream.net';
+
+    function getInArgument(x) {
+        if (request && request.inArguments) {
+            for (let i = 0; i < request.inArguments.length; i++) {
+                let e = request.inArguments[i];
+    
+                if (x in e) {
+                    return e[x];
+                }
+            }
+        }
+    }
+
+    const payloadAxios = request;
+
+    payloadAxios    
+        .put(url, payloadAxios)
+        .then(resp => {
+            return res.status(200).json(resp.data);
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).json(error);
+        })
 });
 
 
