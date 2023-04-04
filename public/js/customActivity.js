d25b0085714e6e26b37aadf8b357d60f5b36499a
MNischida <mgonischida@gmail.com>
2023-04-04 17:32:55 -0300

    ajustando eventos

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -6,4 +6,25 @@ define([
     'use strict';
 
     var connection = new Postmonger.Session();
+    var payload = {};
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+
+    function onRender() {
+        connection.trigger('ready');
+    }
+
+    function initialize(data) {
+        if (data) {
+            payload = data;
+        }
+    }
+
+    function save() {
+        payload["arguments"].execute.inArguments = [{ message: value }];
+        payload["metaData"].isConfigured = true;
+        connection.trigger("updateActivity", payload);
+    }
 })
