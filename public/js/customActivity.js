1cb57201d0e42dc1552237861bd40bcd36cb5420
MNischida <mgonischida@gmail.com>
2023-04-04 20:02:49 -0300

    fazendo requisicao

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -17,25 +17,50 @@ define([
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
+    connection.on("requestedTokens", onGetTokens);
+    connection.on("requestedEndpoints", onGetEndpoints);
 
     connection.on("clickedNext", onClickedNext);
 
     function onRender() {
         connection.trigger('ready');
+
+        connection.trigger("requestTokens");
+        connection.trigger("requestEndpoints");
     }
 
     function initialize(data) {
         if (data) {
             payload = data;
         }
+
+        var hasInArguments = Boolean(
+            payload["arguments"] &&
+            payload["arguments"].execute &&
+            payload["arguments"].execute.inArguments &&
+            payload["arguments"].execute.inArguments.length > 0
+        );
+      
+        var inArguments = hasInArguments
+            ? payload["arguments"].execute.inArguments
+            : {};
     }
 
     function onClickedNext() {
         save();
       }
 
+      function onGetTokens(tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+      }
+    
+      function onGetEndpoints(endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+      }
+
     function save() {
-        payload["arguments"].execute.inArguments = [{ message: value }];
         payload["metaData"].isConfigured = true;
         connection.trigger("updateActivity", payload);
     }
