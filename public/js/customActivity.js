0313e229003fd2733905786ff8dd0af2e8c6bc41
MNischida <mgonischida@gmail.com>
2023-04-04 21:28:44 -0300

    nao sei o que é

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -9,7 +9,7 @@ define([
     var payload = {};
 
     var steps = [
-        { label: 'Step 1', key: 'step1' }
+        { label: "Step 1", key: "step1" }
     ];
 
     var currentStep = steps[0].key;
@@ -17,30 +17,51 @@ define([
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on("requestedTokens", onGetTokens);
+    connection.on("requestedEndpoints", onGetEndpoints);
 
-    connection.on('clickedNext', onClickedNext);
+    connection.on("clickedNext", onClickedNext);
 
     function onRender() {
         connection.trigger('ready');
 
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
+        connection.trigger("requestTokens");
+        connection.trigger("requestEndpoints");
     }
 
     function initialize(data) {
         if (data) {
             payload = data;
         }
+
+        var hasInArguments = Boolean(
+            payload["arguments"] &&
+            payload["arguments"].execute &&
+            payload["arguments"].execute.inArguments &&
+            payload["arguments"].execute.inArguments.length > 0
+        );
+      
+        var inArguments = hasInArguments
+            ? payload["arguments"].execute.inArguments
+            : {};
     }
 
     function onClickedNext() {
         save();
       }
 
-    function save() {        
-        payload['metaData'].isConfigured = true;
-        connection.trigger('updateActivity', payload);
+      function onGetTokens(tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+      }
+    
+      function onGetEndpoints(endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+      }
+
+    function save() {
+        payload["metaData"].isConfigured = true;
+        connection.trigger("updateActivity", payload);
     }
 })
